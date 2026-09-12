3fb4d9752a6b7e50ea153d3bf98396d7c0941a65
ilya467 <146108205+ilya467@users.noreply.github.com>
2026-09-12 17:09:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -17,4 +17,18 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// ماڈیولر راؤٹ امپورٹ کریں
+const testRoute = require('./routes/test');
+app.use('/test', testRoute);
+
+app.get('/', (req, res) => {
+  res.json({ message: "ہیلو دنیا! ہماری ماڈیولر API کامیابی سے چل رہی ہے!" });
 });
+
+// اہم: Vercel کے لیے app.listen کو ہٹا کر نیچے والا ایکسپورٹ لکھنا ضروری ہے
+module.exports = app;
